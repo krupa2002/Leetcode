af6ea2584d07d2cd77726e0d293e2e68a74fd27c
KRUPA DHARAMSHI <90334102+krupa2002@users.noreply.github.com>
2024-06-13 09:58:03 +0530

    Create 2037. Minimum Number of Moves to Seat Everyone.py

---
+++ 2037. Minimum Number of Moves to Seat Everyone.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def minMovesToSeat(self, seats, students):
+        seats.sort()
+        students.sort()
+        ans = 0
+        for i in range(len(seats)):
+            ans += abs(seats[i] - students[i])
+        return ans
